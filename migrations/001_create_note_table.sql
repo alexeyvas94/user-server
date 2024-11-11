f00b42d2d6b7f7bd0d0f062beea5e6d1e9555ead
Alexey Kurnosenkov <alexeyvas94@gmail.com>
2024-11-11 16:36:02 +0300

    two homework

---
+++ migrations/001_create_note_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+CREATE TYPE user_role AS ENUM ('ADMIN', 'USER');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    role user_role DEFAULT 'USER' NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
+DROP TYPE IF EXISTS user_role;
